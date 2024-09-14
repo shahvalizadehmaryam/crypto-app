bf62abe2d7c59236706b8d1eaf0f00196fff7261
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-09-14 13:04:48 +0330

    i added search functionality to search comp and fetched related data based searched query in search comp and cryptoApi FILE.

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,14 +1,33 @@
-const Search = ({currency , setCurrency}) => {
-    return ( 
-        <>
-        <input type="text" />
-        <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
-            <option value="usd">USD</option>
-            <option value="eur">EUR</option>
-            <option value="jpy">JPY</option>
-        </select>
-        </>
-     );
-}
- 
+import { useEffect, useState } from "react";
+import { getSearchedCoins } from "../../services/cryptoApi";
+
+const Search = ({ currency, setCurrency }) => {
+  const [text, setText] = useState("");
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    if (!text) return;
+    const search = async () => {
+      const res = await fetch(getSearchedCoins(text));
+      const json = await res.json();
+
+      if(json.coins) setCoins(json.coins);
+    };
+    search();
+  }, [text]);
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => setText(e.target.value)}
+      />
+      <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
+        <option value="usd">USD</option>
+        <option value="eur">EUR</option>
+        <option value="jpy">JPY</option>
+      </select>
+    </>
+  );
+};
+
 export default Search;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,6 +1,7 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-WURbn3etQEVDMVhHHT2Qu5yw";
-const getCoinList = (page, currency) => {
-  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&locale=en&x_cg_demo_api_key=${API_KEY}`;
-};
-export { getCoinList };
+const getCoinList = (page, currency) =>
+  `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&locale=en&x_cg_demo_api_key=${API_KEY}`;
+const getSearchedCoins = (query) =>
+  `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
+export { getCoinList, getSearchedCoins };
