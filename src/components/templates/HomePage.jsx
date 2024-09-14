34d776c3a6d0131e3fa66f47db68393180ce5ddd
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-09-14 11:48:17 +0330

    I added pagination functionality and also styles to project.

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,24 +1,28 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import TableCoin from "../modules/TableCoin";
 import { getCoinList } from "../../services/cryptoApi";
+import Pagination from "../modules/Pagination";
 
 function HomePage() {
-    const [coins,setCoins] = useState([]);
-    const [isLoading , setIsLoading] = useState(true)
-    useEffect(()=>{
-        const fetchData = async() => {
-         const res = (await fetch(getCoinList()));
-         const json = await res.json();
-         setCoins(json);
-         setIsLoading(false);
-        }
-        fetchData();
-    },[])
+  const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const res = await fetch(getCoinList(page));
+      const json = await res.json();
+      setCoins(json);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [page]);
   return (
     <div>
-        <TableCoin coins={coins} isLoading={isLoading} />
+      <TableCoin coins={coins} isLoading={isLoading} />
+      <Pagination page={page} setPage={setPage} />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
